d9ad5155bffdeee044bcf208491e6a9b38766477
GUNWO0 <kk000624@kyungsung.ac.kr>
2025-04-23 14:59:49 +0900

    수정중

--- src/main/java/com/metacoding/springrocketdanv1/company/CompanyRepository.java
+++ src/main/java/com/metacoding/springrocketdanv1/company/CompanyRepository.java
@@ -4,8 +4,15 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class CompanyRepository {
     private final EntityManager em;
+    
+    public List<Company> findAll() {
+        String q = "SELECT c FROM Company c WHERE c.user.userType = 'company'";
+        return em.createQuery(q, Company.class).getResultList();
+    }
 }
--- src/test/java/com/metacoding/springrocketdanv1/company/CompanyRepositoryTest.java
+++ src/test/java/com/metacoding/springrocketdanv1/company/CompanyRepositoryTest.java
@@ -1,13 +1,28 @@
 package com.metacoding.springrocketdanv1.company;
 
-import com.metacoding.springrocketdanv1.certification.CertificationRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import java.util.List;
+
 @Import(CompanyRepository.class)
 @DataJpaTest
 public class CompanyRepositoryTest {
+
     @Autowired
     private CompanyRepository companyRepository;
+
+    @Test
+    public void findAll_test() {
+        List<Company> companyList = companyRepository.findAll();
+
+        for (Company company : companyList) {
+            System.out.print(company.getId() + ", ");
+            System.out.print(company.getNameKr() + ", ");
+            System.out.print(company.getUser().getUsername());
+            System.out.println();
+        }
+    }
 }
