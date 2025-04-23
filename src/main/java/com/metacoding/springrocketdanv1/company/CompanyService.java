8c12defc65178e26e80507fac003001642b0681b
GUNWO0 <kk000624@kyungsung.ac.kr>
2025-04-23 14:59:49 +0900

    수정중

--- src/main/java/com/metacoding/springrocketdanv1/company/CompanyService.java
+++ src/main/java/com/metacoding/springrocketdanv1/company/CompanyService.java
@@ -3,8 +3,14 @@ package com.metacoding.springrocketdanv1.company;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CompanyService {
     private final CompanyRepository companyRepository;
+
+    public List<Company> 기업리스트() {
+        return companyRepository.findAll();
+    }
 }
