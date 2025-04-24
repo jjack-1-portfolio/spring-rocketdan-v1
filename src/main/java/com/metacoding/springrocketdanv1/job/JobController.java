ddbc5caf9c9a70addc635905369d15250259908a
roni243 <ksri2457@gmail.com>
2025-04-24 15:44:00 +0900

    [feat] 채용 메인 페이지 (#3)
    채용 메인 페이지 완료

--- src/main/java/com/metacoding/springrocketdanv1/job/JobController.java
+++ src/main/java/com/metacoding/springrocketdanv1/job/JobController.java
@@ -2,9 +2,36 @@ package com.metacoding.springrocketdanv1.job;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class JobController {
     private final JobService jobService;
+
+    @GetMapping("/")
+    public String list(Model models, JobResponse.DTO dto) {
+        List<JobResponse.DTO> jobllist = jobService.글목록보기();
+        models.addAttribute("models", jobllist);
+        models.addAttribute("nameKr", dto.getNameKr());
+        return "job/list";
+    }
+
+    @GetMapping("/job/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        // JobDetail을 조회
+        JobResponse.DetailDTO jobDetail = jobService.글상세보기(id);
+
+        // model에 데이터 추가
+        model.addAttribute("jobDetail", jobDetail);
+        model.addAttribute("nameKr", jobDetail.getNameKr());
+        model.addAttribute("salaryRange", jobDetail.getSalaryRange());
+
+        // job/detail 뷰 반환
+        return "job/detail";
+    }
 }
--- src/main/java/com/metacoding/springrocketdanv1/job/JobRepository.java
+++ src/main/java/com/metacoding/springrocketdanv1/job/JobRepository.java
@@ -1,11 +1,24 @@
 package com.metacoding.springrocketdanv1.job;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class JobRepository {
     private final EntityManager em;
+
+    public List<Job> findAll() {
+        String sql = "SELECT * FROM job_tb";  // 네이티브 SQL 쿼리
+        Query query = em.createNativeQuery(sql, Job.class);
+        return query.getResultList();
+    }
+
+    public Job findById(Integer id) {
+        return em.find(Job.class, id);
+    }
 }
